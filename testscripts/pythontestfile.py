0c8379e69ab9fa242c6fd45d3a65522690473841
unknown <ksdaftari@gmail.com>
2018-09-01 00:39:00 -0400

    adding transition matrix for markov process convergence function in r

--- testscripts/pythontestfile.py
+++ testscripts/pythontestfile.py
@@ -1,11 +1,6 @@
-from prettytable import PrettyTable
+for x in range(10):
+    x
+x = range(10)
 
-t = PrettyTable(['number', 'othernumber'])
-for n in range(1, 10):
-    t.add_row([n, n+5])
-print(t)
-
-# stuff
-# another
-# comment
-# hello
+for i in x:
+    print(i)
